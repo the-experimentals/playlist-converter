7c9212cfb3448c0e9400fa59d5e85af6c17f6f7f
Bibek Saini <itsbibeksaini@gmail.com>
2023-07-18 14:41:41 -0400

    WIP: client app gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,14 @@ plugins {
     kotlin("jvm") version "1.9.0"
     id("io.spring.dependency-management") version "1.1.2"
     id("org.springframework.boot") version "3.1.1" apply false
+    id("com.github.node-gradle.node") version "5.0.0" apply false
 }
 
 allprojects {
     group = "com.playlist.converter"
 
     apply {
-        plugin("kotlin")
         plugin("io.spring.dependency-management")
-    }
-
-    tasks{
-        test{
-            useJUnitPlatform()
-        }
     }
 
     afterEvaluate {
