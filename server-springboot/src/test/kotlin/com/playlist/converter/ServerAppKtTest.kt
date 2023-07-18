6de44485f2db97a059c2d4f8045b741099d83bb1
Bibek Saini <itsbibeksaini@gmail.com>
2023-07-18 01:06:00 -0400

    WIP: application context load test

--- server-springboot/src/test/kotlin/com/playlist/converter/ServerAppKtTest.kt
+++ server-springboot/src/test/kotlin/com/playlist/converter/ServerAppKtTest.kt
@@ -1,5 +1,15 @@
 package com.playlist.converter
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 
-class ServerAppKtTest
+@SpringBootTest
+class ServerAppKtTest {
+    @Test
+    fun contextLoad(context: ApplicationContext){
+        assertNotNull(context)
+    }
+}
+
