bde10d4135270a650c3e519401d0893d6163c9c7
Bibek Saini <itsbibeksaini@gmail.com>
2023-07-30 00:26:24 -0400

    Update ConverterController.kt

--- converter-service/src/main/kotlin/com/playlist/converter/features/converter/api/v1/controllers/ConverterController.kt
+++ converter-service/src/main/kotlin/com/playlist/converter/features/converter/api/v1/controllers/ConverterController.kt
@@ -1,9 +1,11 @@
 package com.playlist.converter.features.converter.api.v1.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/v1")
+@RestController()
+@RequestMapping("/api/v1")
 class ConverterController {
 
     @GetMapping("authorize")
