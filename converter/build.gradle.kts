fa15d7f084d49635e4891898affa5e834c49f065
Bibek Saini <itsbibeksaini@gmail.com>
2023-07-29 23:33:42 -0400

    updates

--- converter/build.gradle.kts
+++ converter/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins{
     id("io.freefair.lombok")
 }
+
+dependencies{
+    implementation("org.springframework.boot:spring-boot-starter-web"){
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-undertow")
+
+}
+
