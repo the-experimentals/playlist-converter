2f4dc5a2f654962e547bf633f8146a461b75a201
Bibek Saini <itsbibeksaini@gmail.com>
2023-07-30 01:13:45 -0400

    client app

--- clientapp-react/src/App.tsx
+++ clientapp-react/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import {Card, Grid} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+ss
+      </Grid>
+      <Grid item xs={4}>
+          <Card className='shadow'>
+                dasda
+          </Card>
+      </Grid>
+    </Grid>
   );
 }
 
