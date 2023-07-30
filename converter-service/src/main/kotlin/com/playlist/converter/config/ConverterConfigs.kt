2d8e0e20b685f2c3e22017ee2a6cfc012ea48db4
Bibek Saini <itsbibeksaini@gmail.com>
2023-07-29 23:43:37 -0400

    app properties and configs

---
+++ converter-service/src/main/kotlin/com/playlist/converter/config/ConverterConfigs.kt
@@ -0,0 +1,8 @@
+package com.playlist.converter.config
+
+import com.playlist.converter.config.properties.ConverterProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+
+@EnableConfigurationProperties(ConverterProperties::class)
+class ConverterConfigs {
+}
---
+++ converter-service/src/main/kotlin/com/playlist/converter/config/properties/ConverterProperties.kt
@@ -0,0 +1,13 @@
+package com.playlist.converter.config.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "playlist-converter")
+data class ConverterProperties(
+    var spotify: Spotify
+) {
+    data class Spotify(
+        var clientId: String,
+        var clientSecret: String
+    )
+}
